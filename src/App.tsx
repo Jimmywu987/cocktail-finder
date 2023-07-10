105ccc9205f57048e8735e258d94bbe4e238a446
Jimmy Wu <jimmy@wemakeapp.net>
2023-07-10 22:20:41 +0100

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,19 @@ import { Header } from "components/common/Header";
 
 import { Breadcrumb } from "components/common/Breadcrumb";
 import { Router } from "routes/Router";
-import { Head } from "components/common/Head";
 
 const App = () => {
   return (
-    <>
-      <Head />
-      <body className="screen">
-        <Header />
-        <div className="container">
-          <Breadcrumb />
-          <main style={{ minHeight: "90vh" }} className="ui">
-            <Router />
-          </main>
-          <Footer />
-        </div>
-      </body>
-    </>
+    <div className="screen">
+      <Header />
+      <div className="container">
+        <Breadcrumb />
+        <main style={{ minHeight: "90vh" }} className="ui">
+          <Router />
+        </main>
+        <Footer />
+      </div>
+    </div>
   );
 };
 
